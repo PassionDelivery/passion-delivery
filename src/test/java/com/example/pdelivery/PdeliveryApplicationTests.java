f8d88dfd8352cc518e2b0bf92d58bf13a21a378d
hil679 <hil679922@gmail.com>
2026-03-03 13:04:50 +0900

    init commit

---
+++ src/test/java/com/example/pdelivery/PdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
