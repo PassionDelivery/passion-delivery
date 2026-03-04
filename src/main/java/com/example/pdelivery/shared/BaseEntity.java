be855b2ac5477ca193851507beaa550db97057f3
junwoo <jwlee4450@naver.com>
2026-03-04 11:52:13 +0900

    fix: Auditing 설정 애너테이션 추가

--- src/main/java/com/example/pdelivery/shared/BaseEntity.java
+++ src/main/java/com/example/pdelivery/shared/BaseEntity.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.EntityListeners;
@@ -19,7 +21,9 @@ public abstract class BaseEntity extends AbstractEntity {
 	private LocalDateTime createdAt;
 	@CreatedBy
 	private UUID createdBy;
+	@LastModifiedDate
 	private LocalDateTime updatedAt;
+	@LastModifiedBy
 	private UUID updatedBy;
 	private LocalDateTime deletedAt;
 	private UUID deletedBy;
--- src/main/java/com/example/pdelivery/shared/BaseEntityOnlyCreated.java
+++ src/main/java/com/example/pdelivery/shared/BaseEntityOnlyCreated.java
@@ -3,12 +3,17 @@ package com.example.pdelivery.shared;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
 @Getter
 @MappedSuperclass
 public abstract class BaseEntityOnlyCreated extends AbstractEntity {
+	@CreatedDate
 	private LocalDateTime createdAt;
+	@CreatedBy
 	private UUID createdBy;
 }
