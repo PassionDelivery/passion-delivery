2e84c84716c8c5ef4e27e5bdfffaabe10df0581e
안정후(AHN JEONGHU) <afterrest00@gmail.com>
2026-03-04 18:37:43 +0900

    Revert "fix:  빌드 오류"
    This reverts commit 79c2458535cfe79e6b22b6266b620cf1e69e972f.

--- src/main/java/com/example/pdelivery/review/domain/Review.java
+++ src/main/java/com/example/pdelivery/review/domain/Review.java
@@ -2,29 +2,16 @@ package com.example.pdelivery.review.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Embeddable
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Review {
-
-	@Column(name = "rating", nullable = false)
-	private Integer rating;
-
-	@Column(name = "content", nullable = false, length = 200)
-	private String content;
-
-	public Review(Integer rating, String content) {
+public record Review(@Column(name = "rating", nullable = false) Integer rating,
+					 @Column(name = "content", nullable = false, length = 200) String content) {
+	public Review {
 		if (rating < 1 || rating > 5) {
 			throw new IllegalArgumentException("평점은 1 ~ 5점 사이여야 합니다.");
 		}
 		if (content == null || content.isBlank() || content.length() > 200) {
 			throw new IllegalArgumentException("리뷰 내용은 공백이 아니고 또는 200자 이내여야 합니다.");
 		}
-		this.rating = rating;
-		this.content = content;
 	}
 }
