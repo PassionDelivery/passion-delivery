993dbbbe93a63ab7bdc1eb34021a8b1fe940943a
junwoo <jwlee4450@naver.com>
2026-03-04 13:31:05 +0900

    refactor: DB 제약조건 명시코드 추가

--- src/main/java/com/example/pdelivery/review/domain/ReviewEntity.java
+++ src/main/java/com/example/pdelivery/review/domain/ReviewEntity.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import com.example.pdelivery.shared.BaseEntity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -11,9 +12,11 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "p_review")
 public class ReviewEntity extends BaseEntity {
-
+	@Column(name = "order_id", nullable = false, unique = true)
 	private UUID orderId;
+	@Column(name = "customer_id", nullable = false)
 	private UUID customerId;
+	@Column(name = "store_id", nullable = false)
 	private UUID storeId;
 	@Embedded
 	private Review review;
