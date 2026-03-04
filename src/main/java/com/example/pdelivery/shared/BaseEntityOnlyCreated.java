1e4ffc5aa6269cdc0b2d0bb7f28406af51b5aa62
junwoo <jwlee4450@naver.com>
2026-03-04 12:12:42 +0900

    fix: @EntityListeners 누락 수정

--- src/main/java/com/example/pdelivery/shared/BaseEntityOnlyCreated.java
+++ src/main/java/com/example/pdelivery/shared/BaseEntityOnlyCreated.java
@@ -5,12 +5,15 @@ import java.util.UUID;
 
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
 @Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntityOnlyCreated extends AbstractEntity {
 	@CreatedDate
 	private LocalDateTime createdAt;
