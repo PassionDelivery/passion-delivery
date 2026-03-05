a4c6e292c6d6dd3d782e62c7bf84aad86ede7221
junwoo <jwlee4450@naver.com>
2026-03-05 15:38:41 +0900

    refactor: 커스텀 에러 처리를 위한 코드 수정

--- src/main/java/com/example/pdelivery/review/domain/Review.java
+++ src/main/java/com/example/pdelivery/review/domain/Review.java
@@ -1,5 +1,9 @@
 package com.example.pdelivery.review.domain;
 
+import static com.example.pdelivery.review.ReviewErrorCode.*;
+
+import com.example.pdelivery.review.ReviewException;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
@@ -8,10 +12,10 @@ public record Review(@Column(name = "rating", nullable = false) Integer rating,
 					 @Column(name = "content", nullable = false, length = 200) String content) {
 	public Review {
 		if (rating < 1 || rating > 5) {
-			throw new IllegalArgumentException("평점은 1 ~ 5점 사이여야 합니다.");
+			throw new ReviewException(REVIEW_RATING_BOUNDED_ERROR);
 		}
 		if (content == null || content.isBlank() || content.length() > 200) {
-			throw new IllegalArgumentException("리뷰 내용은 공백이 아니고 또는 200자 이내여야 합니다.");
+			throw new ReviewException(REVIEW_CONTENT_SIZE_ERROR);
 		}
 	}
 }
