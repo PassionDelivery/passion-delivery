60370f9cec983988c222c3fab9b96aab0bfe47a2
hil679 <hil679922@gmail.com>
2026-03-05 16:45:04 +0900

    fix: provider and requirer - provider는 담당자가 할 수 있도록 삭제 후 로직에 영향 안 가게 수정

--- src/main/java/com/example/pdelivery/order/infrastructure/required/cart/CartData.java
+++ src/main/java/com/example/pdelivery/order/infrastructure/required/cart/CartData.java
@@ -3,9 +3,10 @@ package com.example.pdelivery.order.infrastructure.required.cart;
 import java.util.UUID;
 
 public record CartData(
-	UUID menuId,
-	String menuName,
-	Integer price,
-	Integer quantity
+	UUID storeId,
+	UUID menuId
 ) {
+
 }
+
+
--- src/main/java/com/example/pdelivery/order/infrastructure/required/cart/OrderCartRequirer.java
+++ src/main/java/com/example/pdelivery/order/infrastructure/required/cart/OrderCartRequirer.java
@@ -1,8 +1,7 @@
 package com.example.pdelivery.order.infrastructure.required.cart;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface OrderCartRequirer {
-	public List<CartData> getCartLines(UUID cartId);
+	public CartData getCartLines(UUID cartId);
 }
