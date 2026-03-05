6175fe6a9d780bc6f6a44e42a4dd5c969b9b29b0
안정후(AHN JEONGHU) <afterrest00@gmail.com>
2026-03-05 14:08:18 +0900

    fixup! Update src/main/java/com/example/pdelivery/user/presentation/dto/SignupRequestDto.java - 불필요한 이스케이프 삭제

--- src/main/java/com/example/pdelivery/user/presentation/dto/SignupRequestDto.java
+++ src/main/java/com/example/pdelivery/user/presentation/dto/SignupRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.pdelivery.user.presentation.dto;
 
 import com.example.pdelivery.user.domain.entity.UserRole;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -17,8 +18,8 @@ public class SignupRequestDto {
 	private String username;
 
 	@NotBlank
-	`@Pattern`(
-		regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?])[A-Za-z\\d!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]{8,15}$",
+	@Pattern(
+		regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?])[A-Za-z\\d!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]{8,15}$",
 		message = "password는 8~15자, 대소문자·숫자·특수문자를 모두 포함해야 합니다."
 	)
 	private String password;
